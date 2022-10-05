3a7e49b211e62a9ef12325f076d6467a1a950e81
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2022-10-05 10:13:52 +0200

    lesson 52

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,13 @@
+import './NewExpense.css';
+import React from 'react';
+import ExpenseForm from './ExpenseForm';
+
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+};
+
+export default NewExpense;
